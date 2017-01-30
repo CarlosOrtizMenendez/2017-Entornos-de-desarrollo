910cf5b815176c3e41d16a3fc0470a8d964804bd
Carlos Ortiz <compalortizmenendez@gmail.com>
2017-01-30 16:57:37 +0100

    1ª Version Prueba GitHub

---
+++ Prueba GitHub/src/PruebaGitHub.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class PruebaGitHub {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		byte n;
+		n = in.nextByte();
+		if (n > 0 && n <= 5) {
+			do {
+				System.out.println("Hola mundo.");
+				n -= 1;
+			} while (n > 0 && n <= 5);
+		}
+
+	}
+
+}
